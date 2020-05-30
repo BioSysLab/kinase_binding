b4c9a63405f1220318a53abfb9a645514d04b206
Panagiotis Terzopoulos <panos@deepcure.ai>
2020-05-30 06:41:05 -0400

    minor updates on the spliting code and splits for akt1 and pi3k

--- learning/splts.py
+++ learning/splts.py
@@ -27,9 +27,10 @@ class BiolabSplitter:
     def create_folds(self):
         used_mols = []
         folds = []
+        fold_size = int(len(self.distance_matrix) / self.k)
         for _ in tqdm(range(self.k - 1)):
             new_dm = self.distance_matrix.drop(used_mols).drop(columns=used_mols)
-            mols_added = self.create_group(new_dm, int(len(self.distance_matrix) / self.k))
+            mols_added = self.create_group(new_dm, fold_size)
             used_mols += mols_added
             folds.append(mols_added)
         final_group = self.distance_matrix.drop(used_mols).drop(columns=used_mols).index
@@ -43,4 +44,25 @@ class BiolabSplitter:
 
             assert len(set(f)) == len(f), 'Whoops. There are duplicates inside the folds. Something went wrong.'
 
+        # return the folds in the form [(test_0, val_0), (test_1, val_1),..., (test_k, val_k)]
+        folds = [(self.distance_matrix.index.difference(f), f) for f in folds]
         return folds
+
+
+def create_test_set_from_folds(folds: list, i: int):
+    """
+    Gets a list of k folds in the form [(test_0, val_0), (test_1, val_1),..., (test_k, val_k)] and a number of fold i
+    and separates this fold as test set ensuring no data leakage.
+
+    Returns:
+        The new list of train_val sets without the test set and the (all_train_set, test_set) tuple.
+    """
+    assert i < len(folds)
+    test = folds[i][1]
+    train = folds[i][0]
+    new_folds = []
+    for k, fold in enumerate(folds):
+        if k != i:
+            new_folds.append((fold[0].intersection(train), fold[1].intersection(train)))
+
+    return new_folds, (train, test)
